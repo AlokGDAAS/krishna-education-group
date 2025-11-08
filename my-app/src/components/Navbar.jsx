a626efa1f656c9d178be24de2c92075d606489d6
AlokGDAAS <ankit25792kumar@gmail.com>
2025-11-08 22:56:27 +0530

    mobile menu added

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { LuSquareMenu } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenu, setIsMenu] = useState(false);
+
+  const closeMenu = () => setIsMenu(false);
+
   return (
     <div className="bg-[var(--primary)]">
-      <div className="max-w-[90vw]  md:flex md:justify-between items-center mx-auto">
-        <div className="flex items-center">
+      <div className="md:max-w-[90vw]  md:flex md:justify-between items-center md:mx-auto px-2 md:px-0">
+        <div className="flex items-center justify-between">
           <div>
-            <img src="./logo.png"  alt="" className="w-30 bg-white/50 rounded-lg m-2"/>
+            <img
+              src="./logo.png"
+              alt=""
+              className="w-20 md:w-30 bg-white/50 rounded-lg m-2"
+            />
           </div>
           <p className="font-bold text-lg md:text-xl text-white text-center md:text-start">
             Krishna Education Group
           </p>
+          <div className="md:hidden text-3xl text-white pr-4"></div>
+          <div
+            onClick={() => setIsMenu((prev) => !prev)}
+            className="md:hidden cursor-pointer z-50 text-white"
+          >
+            {isMenu ? <IoClose size={30} /> : <LuSquareMenu size={30} />}
+          </div>
         </div>
 
         <div className="hidden md:block">
@@ -31,6 +49,37 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+
+      {/* Mobile Menu Drawer */}
+      {isMenu && (
+        <div className="absolute top-[9vh] right-0 w-2/5 bg-[#17192eff] p-6 flex flex-col gap-5 md:hidden z-40 rounded-lg shadow-lg">
+          <Link to="/" onClick={closeMenu} className="cursor-pointer m-link">
+            Home
+          </Link>
+          <Link
+            to="about"
+            onClick={closeMenu}
+            className="cursor-pointer m-link"
+          >
+           About
+          </Link>
+          <Link
+            to="study-mat"
+            onClick={closeMenu}
+            className="cursor-pointer m-link"
+          >
+            Study-Material
+          </Link>
+          <Link
+            to="up-ex"
+            onClick={closeMenu}
+            className="cursor-pointer m-link"
+          >
+            Upcoming-Examination
+          </Link>
+
+        </div>
+      )}
     </div>
   );
 };
