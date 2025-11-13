4636994e4b7ef5e6a23cbfe107ebde811d4a540e
AlokGDAAS <ankit25792kumar@gmail.com>
2025-11-13 22:21:28 +0530

    rd

--- my-app/src/components/ImageSlider.jsx
+++ my-app/src/components/ImageSlider.jsx
@@ -15,111 +15,99 @@ export default function Main_slider_1() {
 
   const data = [
     {
-      id:1,
-      img:"shivam.jpg",
-      deg:"",
-      name:"Shivam Pundhir",
-      alt:""
-
+      id: 1,
+      img: "Manoj_2.jpg",
+      deg: "",
+      name: "Manoj Tiwari",
+      alt: "",
     },
     {
-      id:2,
-      img:"Bharat_2.jpg",
-      deg:"",
-      name:"Bharat gautam",
-      alt:""
-
+      id: 2,
+      img: "nutan_rana.jpg",
+      deg: "",
+      name: "Nutan Rana",
+      alt: "",
     },
     {
-      id:3,
-      img:"Manoj_2.jpg",
-      deg:"",
-      name:"Manoj",
-      alt:""
+      id: 3,
 
+      img: "Alok.jpg",
+      deg: "",
+      name: "Alok Kumar",
+      alt: "",
+    },
+
+    {
+      id: 4,
+      img: "Deepak_pathak.jpg",
+      deg: "",
+      name: "Deepak Pathak",
+      alt: "",
     },
     {
-      id:4,
-      img:"Deepak_pathak.jpg",
-      deg:"",
-      name:"Deepak Pathak",
-      alt:""
-
+      id: 5,
+      img: "shyam_s_sharma.jpg",
+      deg: "",
+      name: "Shyam Sundar Sharma",
+      alt: "",
     },
     {
-      id:5,
-      img:"shyam_s_sharma.jpg",
-      deg:"",
-      name:"Shyam Sundar Sharma",
-      alt:""
-
+      id: 6,
+      img: "kamal_p.jpg",
+      deg: "",
+      name: "Kamal Prakash",
+      alt: "",
     },
     {
-      id:6,
-      img:"kamal_p.jpg",
-      deg:"",
-      name:"Kamal Prakash",
-      alt:""
-
+      id: 7,
+      img: "shivam.jpg",
+      deg: "",
+      name: "Shivam Pundhir",
+      alt: "",
     },
     {
-      id:7,
-      img:"nutan_rana.jpg",
-      deg:"",
-      name:"Nutan Rana",
-      alt:""
-
+      id: 8,
+      img: "Gopal_pathak.jpg",
+      deg: "",
+      name: "Gopal Pathak",
+      alt: "",
     },
     {
-      id:8,
-      img:"Gopal_pathak.jpg",
-      deg:"",
-      name:"Gopal Pathak",
-      alt:""
-
+      id: 9,
+      img: "rajat_kausik.jpg",
+      deg: "",
+      name: "Rajat Kaushik",
+      alt: "",
     },
     {
-      id:9,
-      img:"rajat_kausik.jpg",
-      deg:"",
-      name:"Rajat Kausik",
-      alt:""
-
+      id: 10,
+      img: "Bharat_2.jpg",
+      deg: "",
+      name: "Bharat gautam",
+      alt: "",
     },
     {
-      id:10,
-      img:"Alok.jpg",
-      deg:"",
-      name:"Alok Kumar",
-      alt:""
-
+      id: 11,
+      img: "sanjeev.jpg",
+      deg: "",
+      name: "Sanjeev Parihar",
+      alt: "",
     },
     {
-      id:11,
-      img:"sanjeev.jpg",
-      deg:"",
-      name:"Sanjeev Parihar",
-      alt:""
-
+      id: 12,
+      img: "govind.jpg",
+      deg: "",
+      name: "Govind Yadav",
+      alt: "",
     },
     {
-      id:12,
-      img:"govind.jpg",
-      deg:"",
-      name:"Govind Yadav",
-      alt:""
-
+      id: 13,
+      img: "narendra.jpg",
+      deg: "",
+      name: "Narendra Rajput",
+      alt: "",
     },
-    {
-      id:13,
-      img:"narendra.jpg",
-      deg:"",
-      name:"Narendra Rajput",
-      alt:""
-
-    },
-    
-  ]
+  ];
 
   return (
     <div className="flex flex-col items-center px-2">
@@ -134,7 +122,9 @@ export default function Main_slider_1() {
           nextEl: ".custom-next",
           prevEl: ".custom-prev",
         }}
-        onSwiper={(swiper) => setTotalSlides(swiper.slides.length - swiper.loopedSlides * 2)}
+        onSwiper={(swiper) =>
+          setTotalSlides(swiper.slides.length - swiper.loopedSlides * 2)
+        }
         onSlideChange={(swiper) => {
           let realIndex = swiper.realIndex + 1;
           setCurrentSlide(realIndex);
@@ -165,20 +155,16 @@ export default function Main_slider_1() {
       >
         {data.map((item, i) => (
           <SwiperSlide key={i}>
-         
             <img
               src={item.img}
               alt={`slide_${i}`}
               className="rounded-lg mx-auto w-full shadow-lg border border-gray-300  object-cover aspect-1/1"
             />
-           
 
             <div className="mt-2 text-white">
-            <p className="text-center text-sm md:text-base">{item.name} </p>
-            <p className="text-center text-sm md:text-base">{item.deg}</p>
+              <p className="text-center text-sm md:text-base">{item.name} </p>
+              <p className="text-center text-sm md:text-base">{item.deg}</p>
             </div>
-
-            
           </SwiperSlide>
         ))}
       </Swiper>
@@ -194,12 +180,9 @@ export default function Main_slider_1() {
         </div>
 
         <button className="custom-next bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-600 transition text-sm md:text-base">
-           ▶
+          ▶
         </button>
       </div>
     </div>
   );
 }
-
-
-
