6c561712ac546b637906cca2d95fe8cbb3820b31
AlokGDAAS <ankit25792kumar@gmail.com>
2025-11-19 23:25:55 +0530

    modern history tesr added

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className='bg-black/70 p-8'>
-
+      <div className='bg-black/70 p-8 text-white text-center'>
+         <p>Copyright © 2025 Alok-Dev. All rights reserved.</p>
       </div>
     </div>
   )
--- my-app/src/pages/Study_material.jsx
+++ my-app/src/pages/Study_material.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Study_material = () => {
   return (
     <div className="md:flex justify-center mt-6 gap-4 font-semibold text-gray-700 text-sm">
-      {/* <div className="p-4 rounded-lg shadow-md bg-white flex flex-col items-center">
-        <a href="/m_h_test.pdf" target="_blank" rel="noopener noreferrer" className="w-fil ">
+      <div className="p-4 rounded-lg shadow-md bg-white flex flex-col items-center">
+        <a href="/Modern_History_Test.pdf" target="_blank" rel="noopener noreferrer" className="w-fil ">
           <img
             src="./pdf_logo.png"
             alt="pdf-logo"
             className="w-24 cursor-pointer hover:scale-105 transition "
           />
         </a>
-        <p className="pt-2"> Modern history test</p>
-      </div> */}
+        <p className="pt-2"> Modern History Test</p>
+      </div>
 
     </div>
   );
