f7cc72e0c91eeb1d3a318517441199e5aa8e309a
AlokGDAAS <ankit25792kumar@gmail.com>
2025-11-19 21:25:47 +0530

    vdeo frame edited

--- my-app/src/components/YoutubeVedios/VedioLec.jsx
+++ my-app/src/components/YoutubeVedios/VedioLec.jsx
@@ -7,15 +7,16 @@ const VedioLec = () => {
         {
             id:1,
             src:"https://www.youtube.com/embed/J3BZvLIHm_0",
-            title:"YouTube video"
+            title:"YouTube video",
+            heading:"Sagar nital prasaran siddhant"
         }
     ]
   return (
-    <div>
+    <div className=' p-2 rounded-lg shadow-lg'>
         <div>
             {vedios_data.map((item , i)=>(
                 <div key={i}>
-                  <YoutubePlayer src={item.src} title={item.title}/>
+                  <YoutubePlayer src={item.src} title={item.title} heading={item.heading}/>
                 </div>
             ))}
         </div>
--- my-app/src/components/YoutubeVedios/YoutubePlayer.jsx
+++ my-app/src/components/YoutubeVedios/YoutubePlayer.jsx
@@ -1,14 +1,19 @@
-const YoutubePlayer = ({src,title,}) => {
+const YoutubePlayer = ({src,title,heading}) => {
   return (
+    <div className="bg-white md:w-5/12 mx-auto p-2 rounded-lg shadow-lg">
+      <h1 className=" text-xs md:text-sm font-medium pb-2">{heading}</h1>
     <div  className="rounded">
       <iframe
         src={src}
         title={title}
-        className="md:w-1/3 aspect-16/9 rounded shadow-lg"        
+        className=" aspect-16/9 rounded shadow-lg"        
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
     </div>
+
+    </div>
+
   );
 };
 
