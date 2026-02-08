2cc80bf7b7580e53f87185f53948599c90b96179
AlokGDAAS <ankit25792kumar@gmail.com>
2026-02-08 21:20:25 +0530

    rd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,6 +17,7 @@ import Main_slider_1 from "./components/ImageSlider";
 import O_level_quiz_2 from "./components/quiz/O_level_quiz_2";
 import O_level_quiz_2_read_mode from "./components/quiz/O_level_quiz_2_read_mode";
 import O_level_quiz_2_theory from "./components/quiz/O_level_quiz_2_theory";
+import O_level_quiz_python from "./components/quiz/O_level_quiz_python";
 
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
           <Route path="/practice-test/:slug" element={<Quiz/>} />
           <Route path="/practice-test-o-level/:slug" element={<O_level_quiz/>} />
           <Route path="/practice-test-o-level_2/:slug" element={<O_level_quiz_2/>} />
+          <Route path="/practice-test-o-level_python/:slug" element={<O_level_quiz_python/>} />
           <Route path="/practice-test-o-level_2_read_mode/:slug" element={<O_level_quiz_2_read_mode/>} />
           <Route path="/practice-test-o-level_2_read_mode_theory/:slug" element={<O_level_quiz_2_theory/>} />
           <Route path="/practice-test-uppsc/:slug" element={<UPPSC_quiz/>} />
---
+++ my-app/src/components/quiz-data/O_level_quiz_python_data.jsx
@@ -0,0 +1,13 @@
+export const o_level_quiz_python_en_part_1 = [
+  {
+    id: 1,
+    question: "Which is not a valid Background property of CSS ?",
+    options: [
+      "background-image",
+      "background-position",
+      "background-color",
+      "background-iterate"
+    ],
+    answer: "background-iterate",
+    topic: "CSS"
+  },]
